e611ca58797f8314220b0554bfed932b51b2ca37
Anibal Fuentes <anibaldfuentesprieto@gmail.com>
2024-04-08 14:11:49 -0500

    NavigationBar Ok

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:moo_app/utils/routes.dart';
+//import 'package:get/get.dart';
 
 void main() => runApp(const MyApp());
 
@@ -8,6 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Moo App',
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:moo_app/widgets/button_nav_bar.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Bandeja principal',
-          style: TextStyle(
-            color: Colors.white,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.green.shade800,
+        title: const Text('Home'),
       ),
       body: const Center(
-        child: Text('Contenido de la página'),
+        child: Text('Contenido del Home'),
       ),
-      bottomNavigationBar: const BottomNavBarWidget(),
     );
   }
 }
